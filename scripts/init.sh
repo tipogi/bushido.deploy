1b624ca80d7a19b19cd15305491d047ddfef8480
WhoKnows <who@knows.eus>
2022-10-31 10:13:17 +0100

    Change init script commands

--- scripts/init.sh
+++ scripts/init.sh
@@ -30,15 +30,17 @@ cloneRepositories()
     # Access to services folder and clone and delete all github 
     cd "$BUSHIDO_DOCKER_PATH"
     git clone "$BUSHIDO_GUIDE_GITHUB"
+    printInsideBlockOutput "bushido.guide clonned!"
     git clone "$BUSHIDO_BACKEND_GITHUB"
     # Change name to bushido backend repository
     mv bushido-backend bushido.backend
+    printInsideBlockOutput "bushido.backend clonned!"
     mkdir -p "${BUSHIDO_WEB_SERVER_PATH}" 
     mkdir -p "${BUSHIDO_DDBB_PATH}"
     git clone "$BUSHIDO_MARKET_GITHUB"
+    printInsideBlockOutput "bushido.market clonned!"
     git clone "$BUSHIDO_LIBRARY_GITHUB"
-    printInsideBlockOutput "bushido services clonned!"
-    mkdir -p $(echo "${DOCKER_CONTAINER_PATH}/${APP_FOLDER}")
+    printInsideBlockOutput "bushido.library clonned!"
     printBlockFinishOutput "Repositories cloned in its path"
 }
 
@@ -50,15 +52,15 @@ copyTheServicesParameters()
     cp -v $(echo "${APP_PARAM_PATH}/config.constants.ts") $(echo "${BUSHIDO_GUIDE_PATH}/src/constants")
     # Import ddbb (app) environment files
     printInsideBlockOutput "copy ddbb environment files..."
-    cp -v $(echo "${DB_PARAM_PATH}/neo4j") $(echo "${BUSHIDO_DDBB_PATH}/")
+    cp -r -v $(echo "${DB_PARAM_PATH}/neo4j") $(echo "${BUSHIDO_DDBB_PATH}/")
     cp -v $(echo "${DB_PARAM_PATH}/.env") $(echo "${BUSHIDO_DDBB_PATH}/")
     # Import ddbb (app) environment files
     printInsideBlockOutput "copy bushido web server files..."
-    cp -v $(echo "${WEB_SERVER_PARAM_PATH}/config") $(echo "${BUSHIDO_WEB_SERVER_PATH}/")
+    cp -r -v $(echo "${WEB_SERVER_PARAM_PATH}/config") $(echo "${BUSHIDO_WEB_SERVER_PATH}/")
     printImportantMessage "DO NOT FORGET TO IMPORT .env, Dockerfile and init.sh after bundle creation"
     # Import bushido.library environment file
     printInsideBlockOutput "copy bushido.library environment files..."
-    cp -v $(echo "${LIBRARY_PARAM_PATH}/config") $(echo "${BUSHIDO_LIBRARY_PATH}/")
+    cp -r -v $(echo "${LIBRARY_PARAM_PATH}/config") $(echo "${BUSHIDO_LIBRARY_PATH}/")
      # Import bushido.market docker files
     printInsideBlockOutput "copy bushido.library docker files..."
     cp -v $(echo "${NOKYC_PARAM_PATH}/Dockerfile") $(echo "${BUSHIDO_MARKET_PATH}/etc/fastapi")
@@ -72,7 +74,7 @@ read -p "Would you like to init bushido.guide application? (Y or N): " x
 if [[ "$x" = "y" &&  $EUID -ne 0 ]]; then
     highlightOutput $(whoami) ${EUID} "running the script and its id is"
     echo "Do you want to clone or copy environment files? (clone or env):"
-    read -p "NOTE: If you decide to copy environment files, first edit all the files acording the environment that you want to setdo not edit the environment files: " y
+    read -p "NOTE: If you decide to copy environment files, first edit all the files acording the environment that you want to set, do not edit the environment files: " y
     if [[ "$y" = "clone" &&  $EUID -ne 0 ]]; then
         createAllTheFolders
         cloneRepositories
