4e836ae6d540bb55cfeb2853032056e0d9c84018
WhoKnows <who@knows.eus>
2022-10-31 12:51:37 +0100

    small changes

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,32 +6,35 @@ source path.sh
 updateAllRepositories()
 {
   printBlockStartOutput "Update application layer repositories"
-    # Access to services folder and clone and delete all github 
-    cd "$BUSHIDO_GUIDE_PATH"
-    git pull "$BUSHIDO_GUIDE_GITHUB"
-    printInsideBlockOutput "bushido.guide updated!"
-    cd "$BUSHIDO_BACKEND_PATH"
-    git pull "$BUSHIDO_BACKEND_GITHUB"
-    printInsideBlockOutput "bushido.backend updated!"
-    cd "$BUSHIDO_MARKET_PATH"
-    git pull "$BUSHIDO_MARKET_GITHUB"
-    printInsideBlockOutput "bushido.market updated!"
-    cd "$BUSHIDO_LIBRARY_REPO_PATH"
-    git pull "$BUSHIDO_LIBRARY_GITHUB"
-    printInsideBlockOutput "bushido.library updated!"
+  # Access to services folder and clone and delete all github 
+  cd "$BUSHIDO_GUIDE_PATH"
+  git pull "$BUSHIDO_GUIDE_GITHUB"
+  printInsideBlockOutput "bushido.guide updated!"
+  cd "$BUSHIDO_BACKEND_PATH"
+  git pull "$BUSHIDO_BACKEND_GITHUB"
+  printInsideBlockOutput "bushido.backend updated!"
+  cd "$BUSHIDO_MARKET_PATH"
+  git pull "$BUSHIDO_MARKET_GITHUB"
+  printInsideBlockOutput "bushido.market updated!"
+  cd "$BUSHIDO_LIBRARY_REPO_PATH"
+  git pull "$BUSHIDO_LIBRARY_GITHUB"
+  printInsideBlockOutput "bushido.library updated!"
 }
 
 installNodeDependencies() 
 {
   printBlockStartOutput "Install some repositories node libraries to run the application"
   # Install client node modules
-  npm install "${BUSHIDO_GUIDE_PATH}"
+  cd "$BUSHIDO_GUIDE_PATH"
+  npm install
   printInsideBlockOutput "bushido.guide node_modules installed!"
   # Install bushido web server node modules
-  npm install "${BUSHIDO_BACKEND_PATH}"
+  cd "$BUSHIDO_WEB_SERVER_PATH"
+  npm install
   printInsideBlockOutput "bushido.backend node_modules installed!"
   # Install bushido web server node modules
-  npm install "${BUSHIDO_LIBRARY_ROOT_PATH}"
+  cd "$BUSHIDO_LIBRARY_ROOT_PATH"
+  npm install
   printInsideBlockOutput "bushido.library node_modules installed!"
 }
 
